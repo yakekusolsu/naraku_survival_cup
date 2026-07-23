036a7d8efa70a4d0c6765773e0a07d9d627bccce
よーそつ <kobaka2424@gmail.com>
2026-07-23 17:47:20 +0900

    Restore shop navigation tab

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -20,6 +20,7 @@ const navigation = [
   { label: 'Players', to: '/players' },
   { label: 'News', to: '/news' },
   { label: 'Map', to: '/map' },
+  { label: 'Shop', to: '/shop' },
   { label: 'API', to: '/api-docs' },
 ]
 
